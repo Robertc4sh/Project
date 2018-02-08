4e3913ca14ca0d5189b3f55681666b05b19bac57
Robertc4sh <32402587+Robertc4sh@users.noreply.github.com>
2018-02-08 14:38:05 -0800

    Initial commit

---
+++ Sword.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class Sword here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Sword extends Weapon
+{
+    int x;
+    int y;
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
---
+++ Weapon.java
@@ -0,0 +1,23 @@
+
+/**
+ * Abstract class Weapon - write a description of the class here
+ *
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public abstract class Weapon extends Item
+{
+    int x;
+    int y;
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
